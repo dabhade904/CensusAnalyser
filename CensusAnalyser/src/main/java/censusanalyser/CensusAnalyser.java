1d725eba58cfa0b40863fd275b6073a4f8aaf4dd
dabhade904 <dabhade904@gmail.com>
2020-01-13 17:39:34 +0530

    DELETED : unwanted commented code

--- CensusAnalyser/src/main/java/censusanalyser/CensusAnalyser.java
+++ CensusAnalyser/src/main/java/censusanalyser/CensusAnalyser.java
@@ -18,7 +18,7 @@ import java.util.stream.StreamSupport;
 import static cessusanalyser.CSVBuilderFactory.createCSVBuilder;
 
 public class CensusAnalyser {
-    List<CensusDao> censusList = new ArrayList<CensusDao>();
+    List<CensusDao> censusList = new ArrayList<>();
     List<CSVStateDao> csvStateList = null;
 
     public int loadIndiaCensusData(String csvFilePath) throws CensusAnalyserException {
@@ -31,29 +31,6 @@ public class CensusAnalyser {
         return censusList.size();
     }
 
-//    private List<CensusDao> loadCensusData(String csvFilePath, Class<CensusLoader> censusLoaderClass) {
-//    }
-
-    /*   private<E> int loadCensusData(String csvFilePath, Class<E> censusCSVClass) throws CensusAnalyserException {
-           try (Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));) {
-               ICSVBuilder csvBuilder = CSVBuilderFactory.createCSVBuilder();
-               List <E> csvFileList = csvBuilder.getCSVFileList(reader, censusCSVClass);
-               if (censusCSVClass.getName().equals("censusanalyser.IndiaCensusCSV")){
-                   csvFileList.stream().filter(censusData -> censusList.add(new CensusDao((IndiaCensusCSV) censusData))).collect(Collectors.toList());
-               } else if(censusCSVClass.getName().equals("censusanalyser.UsCensusCSV")) {
-                   csvFileList.stream().filter(censusData -> censusList.add(new CensusDao((UsCensusCSV)censusData))).collect(Collectors.toList());
-               }
-               return censusList.size();
-           } catch (IOException e) {
-               throw new CensusAnalyserException(e.getMessage(),
-                       CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM);
-           } catch (RuntimeException e) {
-               throw new CensusAnalyserException(e.getMessage(), CensusAnalyserException.ExceptionType.INCORRECT_FILE_DATA);
-           } catch (CSVBuilderException e) {
-               throw new CensusAnalyserException(e.getMessage(), e.type.name());
-           }
-       }
-   */
     public int loadStateData(String csvFilePath) throws CensusAnalyserException {
         try (Reader reader = Files.newBufferedReader(Paths.get(csvFilePath))) {
             ICSVBuilder csvBuilder = createCSVBuilder();
